6a809436c91b6bf15b8bd754db2fc68c5f1d0bbd
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-10-20 21:52:49 -0500

    feat(nutritionists): repositorio JPA con búsqueda por userId

---
+++ src/main/java/pe/edu/upc/center/jameoFit/nutritionists/infrastructure/persistence/jpa/repositories/NutritionistRepository.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.center.jameoFit.nutritionists.infrastructure.persistence.jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.center.jameoFit.nutritionists.domain.model.aggregates.Nutritionist;
+
+import java.util.Optional;
+
+@Repository
+public interface NutritionistRepository extends JpaRepository<Nutritionist, Long> {
+    Optional<Nutritionist> findByUserId(Long userId);
+}
