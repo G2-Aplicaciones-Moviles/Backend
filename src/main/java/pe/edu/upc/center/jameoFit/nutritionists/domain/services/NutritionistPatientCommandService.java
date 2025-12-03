2c0484ddacb6adfdae4e0ba76162a77b0a439508
psfa29 <parodrez29@gmail.com>
2025-12-02 22:14:18 -0500

    feat: add new command delete

--- src/main/java/pe/edu/upc/center/jameoFit/nutritionists/domain/services/NutritionistPatientCommandService.java
+++ src/main/java/pe/edu/upc/center/jameoFit/nutritionists/domain/services/NutritionistPatientCommandService.java
@@ -3,6 +3,7 @@ package pe.edu.upc.center.jameoFit.nutritionists.domain.services;
 import pe.edu.upc.center.jameoFit.nutritionists.domain.model.aggregates.NutritionistPatient;
 import pe.edu.upc.center.jameoFit.nutritionists.domain.model.commands.CreateNutritionistPatientCommand;
 import pe.edu.upc.center.jameoFit.nutritionists.domain.model.commands.ApprovePatientCommand;
+import pe.edu.upc.center.jameoFit.nutritionists.domain.model.commands.DeleteNutritionistPatientCommand;
 
 import java.util.Optional;
 
@@ -17,4 +18,6 @@ public interface NutritionistPatientCommandService {
      * Aprueba una solicitud pendiente
      */
     Optional<NutritionistPatient> handle(ApprovePatientCommand command);
+
+    Optional<NutritionistPatient> handle(DeleteNutritionistPatientCommand command);
 }
